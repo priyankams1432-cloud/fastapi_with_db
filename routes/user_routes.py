2474d5edb362d385e2551ecdafecde4523591f83
priyankams1432-cloud <priyankams1432@gmail.com>
2026-02-06 15:57:08 +0530

    first commit

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from repositories.user_repo import UserRepo
+from fastapi import Depends
+from schemas.user_schema import UserSchema
+
 
 router = APIRouter()
 
 @router.post("/signup")
-def signup():
+def signup(db: Session = Depends(get_db)):
+    user_repo=UserRepo(db)
+    user_repo.add_user()
     return {"message": "user signed up successfully"}
 
 @router.post("/login")
