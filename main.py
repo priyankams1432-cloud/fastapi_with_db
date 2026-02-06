d81eacff7a3e213cd3813bb7f5bef58d0ac70f3e
priyankams1432-cloud <priyankams1432@gmail.com>
2026-02-06 15:03:24 +0530

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from routes.user_routes import router as user_router
 from db import get_db
+from sqlalchemy import create_engine
+import os
+from models import Base
 
 app = FastAPI()
 
 app.include_router(user_router)
 # to create database
+if not os.path.exists("./test.db"):
+    engine = create_engine(DATABASE_URL)
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
